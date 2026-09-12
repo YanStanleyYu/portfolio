3e295ce91d509da0012afe5e893ebe14fd2cd7f0
schua9 <sabrina.h.chua@gmail.com>
2026-09-11 20:55:22 -0400

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
